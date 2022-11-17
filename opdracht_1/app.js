bf057259bcd8fbce6a205bce4341eb7382dbc8af
EbyvanBokkem <9018376@student.zadkine.nl>
2022-11-17 09:37:50 +0100

    opdracht 1.1

--- opdracht_1/app.js
+++ opdracht_1/app.js
@@ -1,29 +1,59 @@
 let paragraaf = document.getElementById("para");
 
 
-const winkelmand = [ "bananen", "whiskey", "chips", "bonen", "aardappelen"];
+const winkelmand = [ "bananen ", "whiskey ", "chips ", "bonen ", "aardappelen "];
 
-// let aantal = winkelmand.length;
 
+let aantal = winkelmand.length;
+
+// opdracht 1
 // paragraaf.innerText = "er zitten " + aantal + " producten in je winkelmand";
 
+// opdrahct 2
+// paragraaf.innerText = "Uw producten zijn: " + winkelmand
+
+// opdracht 3
 // paragraaf.innerText = winkelmand[3] + " staat op de vierde plek in uw winkelmand";
 
+// opdracht 4
 // winkelmand[1] = "bier"
 
 // paragraaf.innerText = winkelmand
 
+// opdracht 5
 // function product_toevoegen(){
 //     winkelmand.push("olifantsappel")
 //     paragraaf.innerText = winkelmand
 // }
 
-function{
-    if (aantal >1)
-    paragraaf.innerText = winkelmand;
-    else (aantal <1) 
-    paragraaf.innerText = "uw heeft geen producten"
-}
+// opdracht 6
+//     if (aantal > 1) {
+//      aantal = winkelmand;
+// }
+//     else { 
+//     aantal = "u heeft niet genoeg producten om te tonen";
+// }
+// document.getElementById("para").innerHTML = aantal;
+// console.log(aantal);
+
+// oprdacht 7
+// if (winkelmand[4] == "drop") {
+// paragraaf.innerText = winkelmand
+// }
+// else{
+//  paragraaf.innerText = "u heeft geen drop"
+// }
+
+// opdracht 8 staat in winkelmand
+
+// opdracht 9
+// const winkelmand2 = winkelmand.slice(2);
+// paragraaf.innerText = winkelmand + " * * " + winkelmand2;
+
+// opdracht 10
+// winkelmand.sort();
+// paragraaf.innerText = winkelmand.sort();
+
 
 
 

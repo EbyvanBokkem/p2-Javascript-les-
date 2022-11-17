e771fb906958ab5c0a06c017243021e9333bed7e
EbyvanBokkem <9018376@student.zadkine.nl>
2022-11-17 08:37:46 +0100

    commit

---
+++ opdracht_1/app.js
@@ -0,0 +1,29 @@
+let paragraaf = document.getElementById("para");
+
+
+const winkelmand = [ "bananen", "whiskey", "chips", "bonen", "aardappelen"];
+
+// let aantal = winkelmand.length;
+
+// paragraaf.innerText = "er zitten " + aantal + " producten in je winkelmand";
+
+// paragraaf.innerText = winkelmand[3] + " staat op de vierde plek in uw winkelmand";
+
+// winkelmand[1] = "bier"
+
+// paragraaf.innerText = winkelmand
+
+// function product_toevoegen(){
+//     winkelmand.push("olifantsappel")
+//     paragraaf.innerText = winkelmand
+// }
+
+function{
+    if (aantal >1)
+    paragraaf.innerText = winkelmand;
+    else (aantal <1) 
+    paragraaf.innerText = "uw heeft geen producten"
+}
+
+
+

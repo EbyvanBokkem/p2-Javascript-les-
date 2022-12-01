f193d2e4efabdf5a9ae485b80cf093a5337fd7f9
EbyvanBokkem <9018376@student.zadkine.nl>
2022-12-01 10:15:33 +0100

    opdracht_3.3

---
+++ opdracht_3.3/app.js
@@ -0,0 +1,15 @@
+// let getal = 20;
+
+// for(let i = 1; i <= 10; i ++){
+//     console.log(i + " x " + getal + " = " + (i * getal))
+// }  
+
+
+ 
+function tafel_van(num){
+    for(let i = 1; i <= 10; i ++){
+      console.log(i + " x " + num + " = " + (i * num))
+    }
+}
+
+tafel_van(12)

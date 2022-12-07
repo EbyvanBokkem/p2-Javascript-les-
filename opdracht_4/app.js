2d35f00f55fc27fb4582bc8ff6df03db88e0874c
EbyvanBokkem <9018376@student.zadkine.nl>
2022-12-07 08:53:59 +0100

    reaction time game

--- opdracht_4/app.js
+++ opdracht_4/app.js
@@ -1,29 +1,29 @@
 const btn = document.getElementById("Go_button");
-const page = document.getElementById("page");
+const page = document.getElementById("page")
 let startTimer;
 let stopTimer;
 
 btn.addEventListener('click', () => {
     btn.style.display = 'none';
     page.style.backgroundColor = "red";
-    setTimeout(function(){
+    setTimeout(function() {
         page.style.backgroundColor = "green";
         startTimer = Date.now()
-    },Math.floor(Math.random() * 9 + 1 * 1000));
+    },Math.floor(Math.random() * 9 + 1 * 1000))
+    if (document.getElementById("time").innerHTML != "") {
+        document.getElementById("time").innerHTML = ""
+    }
 });
 
 page.addEventListener('click', function() {
-
-    if(page.style.backgroundColor == "red" || page.style.backgroundColor == "black") return
+if (page.style.backgroundColor == "green") {
     stopTimer = Date.now();
-    reactionTime =stopTimer - startTimer;
-    document.getElementById("time").innerHTML="Your reaction time is: " + reactionTime + "ms"
-    
- 
-    
+    reactionTime =(stopTimer - startTimer);
+    document.getElementById("time").innerHTML="Your reaction time is:" + reactionTime + "ms";
+    stopTimer = btn.style.display = 'block'
+    stopTimer = page.style.backgroundColor = "black" 
+    }
 })
-
-// had geen zin om die setinterval erin te doen
 
 
 
